a2dfca68d00487f508ddd237576ccba8a94581f0
Pannnomaly <supawith007@gmail.com>
2026-02-06 00:26:19 +0700

    feat(reward): add claim reward API with validation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { GameModule } from './game/game.module';
+import { RewardModule } from './reward/reward.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, GameModule],
+  imports: [PrismaModule, UserModule, GameModule, RewardModule],
   controllers: [AppController],
   providers: [AppService],
 })
