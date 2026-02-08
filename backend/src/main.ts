30c415567644cd241183fc79f042ba8a2832b564
Pannnomaly <supawith007@gmail.com>
2026-02-08 10:58:48 +0700

    chore: add vercel url to cors api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://nextzy-gamification.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
